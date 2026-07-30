464ee952dca71ce746159abf415feece0e35eaf1
Gustavo <gustavoalzarate@gmail.com>
2026-07-29 23:30:09 -0600

    feat: button menu movil added

--- src/components/Nabvar.tsx
+++ src/components/Nabvar.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Menu, XIcon } from 'lucide-react';
 import { LINKS } from '../constants/links';
-import { Menu } from 'lucide-react';
 
 type NavLinkProps = {
   href: string
   children: React.ReactNode
+  isMobile: boolean
 }
 
 function NavLink({
   href,
-  children
+  children,
+  isMobile
 }: NavLinkProps) {
-  const [hovered, setHovered] = useState(false)
+  const [hovered, setHovered] = useState(false);
   return (
     <a
-      className={`text-[12px] tracking-wider uppercase transition-all ${hovered ? 'opacity-100' : 'opacity-45'}`}
+      className={`text-[12px] text-white tracking-wider uppercase transition-all ${hovered || isMobile ? 'opacity-100' : 'opacity-45'}`}
       href={href}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
@@ -25,46 +27,115 @@ function NavLink({
 }
 
 export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   useEffect(() => {
-    const fn = () => setScrolled(window.scrollY > 64)
-    window.addEventListener('scroll', fn)
+    const fn = () => setScrolled(window.scrollY > 64);
+    window.addEventListener('scroll', fn);
     return () => window.removeEventListener('scroll', fn)
   }, [])
 
   return (
-    <nav
-      className="fixed w-full flex justify-between items-center top-0 left-0 right-0 z-100 py-12 px-5"
-      style={{
-        padding: '20px 48px',
-        background: scrolled ? 'rgba(8,8,8,0.88)' : 'transparent',
-        backdropFilter: scrolled ? 'blur(14px)' : 'none',
-        borderBottom: scrolled ? '1px solid var(--border)' : '1px solid transparent',
-        transition: 'all 0.35s ease',
-      }}
-    >
-      <a
-        className="tracking-wider text-fg text-[20px] decoration-0"
-        href="#hero"
+    <div className="relative">
+      <nav
+        className="fixed w-full flex justify-between items-center top-0 left-0 right-0 z-100 py-5! px-12!"
+        style={{
+          background: scrolled ? 'rgba(8,8,8,0.88)' : 'transparent',
+          backdropFilter: scrolled ? 'blur(14px)' : 'none',
+          borderBottom: scrolled ? '1px solid var(--border)' : '1px solid transparent',
+          transition: 'all 0.35s ease',
+        }}
       >
-        GL.<span style={{ color: 'var(--accent)' }}>dev</span>
-      </a>
+        <a
+          className="tracking-wider text-fg text-[20px] decoration-0"
+          href="#hero"
+        >
+          GL.<span style={{ color: 'var(--accent)' }}>dev</span>
+        </a>
 
-      {/* Web */}
-      <div className="hidden md:flex md:justify-end md:gap-4">
-        {LINKS.map(({ label, href }) => (
-          <NavLink key={href} href={href}>
-            {label}
-          </NavLink>
-        ))}
-      </div>
+        <div className="hidden md:flex md:justify-end md:gap-4">
+          {LINKS.map(({ label, href }) => (
+            <NavLink key={href} href={href} isMobile={false}>
+              {label}
+            </NavLink>
+          ))}
+        </div>
 
-      {/* Mobile */}
+        <button
+          onClick={toggleMenu}
+          className="p-2 md:hidden focus:outline-none z-20 cursor-pointer"
+          aria-expanded={showMenu ? "true" : "false"}
+          aria-controls="mobile-menu-dropdown"
+        >
+          <Menu />
+        </button>
+      </nav>
 
-      <div className="flex justify-end md:hidden">
-        <Menu className="size-6 text-white" />
-      </div>
-    </nav>
+      <MobileMenu
+        isOpen={showMenu}
+        toggleMenu={toggleMenu}
+      />
+    </div>
   )
 }
+
+const MobileMenu = ({
+  isOpen,
+  toggleMenu
+}: {
+  isOpen: boolean,
+  toggleMenu: () => void
+}) => {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (isOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+        toggleMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, menuRef]);
+
+  return (
+    <div
+      ref={menuRef}
+      className="relative w-full z-102"
+    >
+      <div
+        id="mobile-menu-dropdown"
+        className={`
+            bg-transparent backdrop-blur-[14px] border-b border-border
+            fixed w-full text-white shadow-xl z-10 
+            transition-all duration-300 ease-out 
+            ${isOpen ? 'opacity-100 translate-y-0 max-h-[90vh]'
+            : 'opacity-0 -translate-y-4 max-h-0 pointer-events-none'
+          }
+        `}
+      >
+        <div className="p-5!">
+          <div className="w-full flex justify-end">
+            <XIcon className="size-5 cursor-pointer" onClick={toggleMenu} />
+          </div>
+          <div className="flex flex-col gap-6 text-white">
+            {LINKS.map(({ label, href }) => (
+              <NavLink key={href} href={href} isMobile={true}>
+                {label}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div >
+    </div >
+  );
+};
+
